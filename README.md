078b5842e04b05239abd952dd3b4897547e31c57
devoptimist <sbrown@chef.io>
2018-08-13 13:20:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Habitat package: filebeat
+
+## Description
+
+Provide a brief description of the `filebeat` plan / purpose.
+
+## Usage
+
+Describe the general usage for the `filebeat` plan
---
+++ plan.sh
@@ -0,0 +1,14 @@
+pkg_name=filebeat
+pkg_origin=devoptimist
+pkg_maintainer="Steve Brown <sbrown@chef.io>"
+pkg_license=("Apache-2.0")
+pkg_version="6.3.1"
+pkg_deps=(core/filebeat)
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  return 0
+}
