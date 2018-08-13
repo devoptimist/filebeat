96337b763b20e6a7d3fab09c1fd9cf127bc4ec00
devoptimist <sbrown@chef.io>
2018-08-13 13:34:11 +0100

    added binds and removed results

--- plan.sh
+++ plan.sh
@@ -4,7 +4,11 @@ pkg_maintainer="Steve Brown <sbrown@chef.io>"
 pkg_license=("Apache-2.0")
 pkg_version="6.3.1"
 pkg_deps=(core/filebeat)
-
+pkg_binds_optional=(
+  [kibana]="port"
+  [elasticsearch]="http-port"
+  [logstash]="port"
+)
 do_build() {
   return 0
 }
